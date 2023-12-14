5a2d4f0cab544ac68c47d722b5dc981fd714a252
shreyu2000 <shreyamdake2000@gmail.com>
2023-12-14 18:32:53 +0530

    added banner and creategroup

---
+++ src/components/Banner.jsx
@@ -0,0 +1,23 @@
+import Lock from "../assets/icons/Lock";
+import banner from "../assets/images/banner.png"
+
+
+function Banner(){
+
+    return(
+    <div className={`${styles.Banner} `}>
+        <div className={styles.center}>
+            <img src={banner}/>
+            <h2>Pocket Notes</h2>
+            <p>Send and receive messages without keeping your phone online.
+Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
+        </div>
+        <div className={styles.encrypt}>
+            <span><Lock/>end-to-end encrypted</span>
+        </div>
+    </div>
+    );
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+}
+
+export default Banner;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from '../components/Banner';
+
+
+
+
+const Home = () => {
+  return (
+    <>
+      <Banner/>
+    </>
+  )
+}
+
+export default Home;
